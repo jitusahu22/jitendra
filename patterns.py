94d2e9116ece98b440f4b82bf3be98d72717c8d9
jitusahu22 <jitendrasahu0018@gmail.com>
2025-03-18 13:37:21 +0530

    Uploaded a python file

---
+++ patterns.py
@@ -0,0 +1,15 @@
+# r=5
+# s=1
+# for i in range(r):
+#     for j in range(s):
+#         print('*',end=' ')
+#     print()
+#     s=s+1
+
+# size = 5
+# for i in range(size):
+#     for j in range(1, size - i):
+#         print(" ", end="")
+#     for k in range(0, i + 1):
+#         print("*", end="")
+#     print()
